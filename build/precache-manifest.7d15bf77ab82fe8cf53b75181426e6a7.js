85e3c4361a888a1ff1475107c30c51e30b9258fc
Mokksh-bhatt <71543314+Mokksh-bhatt@users.noreply.github.com>
2022-01-27 19:02:43 +0530

    Add files via upload

---
+++ build/precache-manifest.7d15bf77ab82fe8cf53b75181426e6a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "828883bda4d9e0c4cc510e1d55cae5e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5387a7aedd5ce027c24",
+    "url": "/static/css/main.9cb7495d.chunk.css"
+  },
+  {
+    "revision": "d4f95a14045cda54fa4d",
+    "url": "/static/js/2.1f054ea5.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/static/js/2.1f054ea5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5387a7aedd5ce027c24",
+    "url": "/static/js/main.d566bf52.chunk.js"
+  },
+  {
+    "revision": "9bc2349bfbe76e87395f",
+    "url": "/static/js/runtime-main.e5afff51.js"
+  }
+]);
